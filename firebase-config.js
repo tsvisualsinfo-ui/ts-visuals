7e45c650159dc7e0f2a8ef6ab091d6fb18cbc075
tsvisualsinfo-ui <tsvisuals.info@gmail.com>
2026-09-12 11:52:37 +0100

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCiKIzcIb_ys1E4f-KHEZhzEYGlqGVcsMA",
+  authDomain: "tsvisualsadmin.firebaseapp.com",
+  projectId: "tsvisualsadmin",
+  storageBucket: "tsvisualsadmin.firebasestorage.app",
+  messagingSenderId: "470316163826",
+  appId: "1:470316163826:web:5bf5ab2243aed9e727e66b",
+  measurementId: "G-XB68BCHSYX"
+};
